eb4afb1741e95454765e0982a77855b54ab96da5
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-05-26 17:47:27 +0900

    Implement check RUNUSER_UID and RUNUSER_GID

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# RUNUSER_UID
+if [ ! -n "${RUNUSER_UID-}" ] ; then
+  echo "RUNUSER_UID is not set or empty"
+  exit 1
+fi
+# RUNUSER_GID
+if [ ! -n "${RUNUSER_GID-}" ] ; then
+  echo "RUNUSER_GID is not set or empty"
+  exit 1
+fi
+
+
 groupmod --gid $RUNUSER_GID galaxy
 usermod --uid $RUNUSER_UID --gid $RUNUSER_GID galaxy
 chown -R $RUNUSER_UID:$RUNUSER_GID /home/galaxy
