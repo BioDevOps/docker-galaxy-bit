0cad279d0735572124925c6516ac7378a53e9ea6
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-05-31 20:31:08 +0900

    Add username setting and SGE_ROOT

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# RUNUSER_USERNAME
+if [ ! -n "${RUNUSER_USERNAME-}" ] ; then
+  echo "RUNUSER_USERNAME is not set or empty"
+  exit 1
+fi
+
 # RUNUSER_UID
 if [ ! -n "${RUNUSER_UID-}" ] ; then
   echo "RUNUSER_UID is not set or empty"
@@ -12,10 +18,9 @@ if [ ! -n "${RUNUSER_GID-}" ] ; then
 fi
 
 
-groupmod --gid $RUNUSER_GID galaxy
-usermod --uid $RUNUSER_UID --gid $RUNUSER_GID galaxy
 chown -R $RUNUSER_UID:$RUNUSER_GID /home/galaxy
+groupmod --gid $RUNUSER_GID galaxy
+usermod --uid $RUNUSER_UID --gid $RUNUSER_GID --login $RUNUSER_USERNAME galaxy
 
-su - galaxy -c "cd /home/galaxy/galaxy-15.07;./run.sh --daemon"
+su - $RUNUSER_USERNAME -c "echo 'hostname; date' | qsub; cd /home/galaxy/galaxy-15.07;SGE_ROOT=/var/lib/gridengine ./run.sh --daemon"
 tail -f /home/galaxy/galaxy-15.07/paster.log
-
